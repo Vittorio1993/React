c8e2c02d74e934056b46ce86e4da561460c70db0
zhanghuakai <zhanghuakai1234@gmail.com>
2022-02-26 22:05:36 +0100

    commit

--- Code_QianFan/myapp/src/01-base/07-ref.js
+++ Code_QianFan/myapp/src/01-base/07-ref.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-25 10:01:35
- * @LastEditTime: 2022-02-25 23:32:04
+ * @LastEditTime: 2022-02-26 22:00:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \myapp\src\01-base\05-事件绑定-01.js
@@ -19,6 +19,7 @@ export default class App extends Component {
                     console.log('click1', this.myref.current.value)
                 }}>add1</button>
             </div >
+
 
         )
     }
