02dcda5504c7efce056ac0a6167edf6d7c2a2e48
zhanghuakai <zhanghuakai1234@gmail.com>
2022-03-08 09:31:27 +0100

    08/03

--- Code_QianFan/myapp/src/01-base/09-循环渲染.js
+++ Code_QianFan/myapp/src/01-base/09-循环渲染.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-28 22:45:51
- * @LastEditTime: 2022-02-28 23:04:17
+ * @LastEditTime: 2022-03-08 09:30:28
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \myapp\src\01-base\09-循环渲染.js
@@ -10,22 +10,35 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
     state = {
-        list: ['111', '222', '333']
+        list: [{
+            id: 1,
+            text: '111'
+        },
+        {
+            id: 2,
+            text: '222'
+        },
+        {
+            id: 3,
+            text: '222'
+        }]
     }
     render() {
-        let newlist = this.state.list.map(item =>
-            <li key={item}>{item}</li>)
+        let newlist = this.state.list.map((item, index) =>
+            <li key={item.id}>{item.text}--{index}</li>)
         return (
             <div>
-                <ui></ui>
-                {
-                    newlist
-                }
-            </ui>
+                <ui>
+                    {
+                        newlist
+                    }
+                </ui>
             </div >
         )
     }
 }
+// 为了列表的复用和重排，设置key值，提高性能
+// 不涉及到列表的增加删除，重排，设置成索引值没有问题
 
 /*
 原生js-map
