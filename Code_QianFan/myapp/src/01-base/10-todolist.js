6706f4dcb7a2127c7e75ac6527b53d59e0170010
zhanghuakai <zhanghuakai1234@gmail.com>
2022-03-21 19:01:46 +0100

    21/03

--- Code_QianFan/myapp/src/01-base/10-todolist.js
+++ Code_QianFan/myapp/src/01-base/10-todolist.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-10 23:07:32
- * @LastEditTime: 2022-03-10 23:31:52
+ * @LastEditTime: 2022-03-21 18:59:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \myapp\src\01-base\10-todolist.js
@@ -33,10 +33,19 @@ export default class App extends Component {
                 <button onClick={this.handleClick2}>add2</button>
                 <ul>
                     {
-                        this.state.list.map(item =>
-                            <li key={item.id}>{item.mytext}</li>)
+                        this.state.list.map((item, index) =>
+                            <li key={item.id}>
+                                {item.mytext}
+                                {/* <button onClick={this.handleClick.bind(this, index)}>del</button> */}
+                                <button onClick={() => {
+                                    this.handleClick(index)
+                                }}>del</button>
+                            </li>)
                     }
                 </ul>
+                {/* {!this.state.list.length ? <div>暂无待办事项</div> : null} */}
+
+                {!this.state.list.length && <div>暂无待办事项</div>}
             </div >
 
 
@@ -53,6 +62,19 @@ export default class App extends Component {
             id: Math.random() * 100000,
             mytext: this.myref.current.value
         })
+        this.setState({
+            list: newlist
+        })
+
+        //清空输入框
+        this.myref.current.value = '';
+    }
+
+    handleClick = (index) => {
+        console.log('del-click', index)
+        //不要直接修改状态。可能会造成不可预期的问题
+        let newlist = this.state.list.slice();
+        newlist.splice(index, 1)
         this.setState({
             list: newlist
         })
