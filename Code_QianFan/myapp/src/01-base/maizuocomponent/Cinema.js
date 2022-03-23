44df163ad8185316a4eb48189c4df68197374fbb
zhanghuakai <zhanghuakai1234@gmail.com>
2022-03-23 09:55:59 +0100

    find input

--- Code_QianFan/myapp/src/01-base/maizuocomponent/Cinema.js
+++ Code_QianFan/myapp/src/01-base/maizuocomponent/Cinema.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-21 20:02:42
- * @LastEditTime: 2022-03-22 09:55:57
+ * @LastEditTime: 2022-03-23 09:51:37
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \myapp\src\01-base\maizuocomponent\Cinema.js
@@ -13,6 +13,10 @@ export default class Cinema extends Component {
 
     constructor() {
         super()
+        this.state = {
+            cinemaList: [],
+            backCinemaList: []
+        }
         //axios 第三方用于数据请求的库
         // axios.get().then(res=>{}).catch(err=>{console.log(err)})
         // axios.get('https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=9247391').then(res => {
@@ -27,15 +31,46 @@ export default class Cinema extends Component {
                 'X-Host': 'mall.film-ticket.cinema.list'
             }
         }).then(res => {
-            console.log(res);
+            console.log(res.data.data.cinemas)
+            this.setState({
+                cinemaList: res.data.data.cinemas,
+                backCinemaList: res.data.data.cinemas
+            })
+        }).catch(err => {
+            console.log(err)
         })
     }
 
     render() {
         return (
             <div>
-
+                <input onInput={this.handleInput} />
+                {
+                    this.state.cinemaList.map(item =>
+                        <dl key={item.cinemaId}>
+                            <dt>
+                                {item.name}
+                            </dt>
+                            <dd>
+                                {item.address}
+                            </dd>
+                        </dl>)
+                }
             </div>
         )
     }
+
+    handleInput = (event) => {
+        console.log('input', event.target.value)
+        // filter 不会影响原数组
+        let newList = this.state.backCinemaList.filter(item => item.name.toUpperCase().includes(event.target.value.toUpperCase()) ||
+            item.address.toUpperCase().includes(event.target.value.toUpperCase()))
+        this.setState({
+            cinemaList: newList
+        })
+    }
 }
+
+
+// filter
+
